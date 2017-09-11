2a68c7032cdf88f0b29c31e75f517d9ca4f669d4
EmaEvidence <emmanuelalabi563@gmail.com>
2017-09-11 10:54:41 +0100

    chore(circleci):integrate circleci with badge
    - add code to create $HOME/reaction folder
    - pend failing tests
    - add badge to readme
    
    [Finishes #150803985]

--- imports/plugins/core/ui-navbar/client/components/navbar/navbar.js
+++ imports/plugins/core/ui-navbar/client/components/navbar/navbar.js
@@ -1,6 +1,7 @@
 import { FlatButton } from "/imports/plugins/core/ui/client/components";
 import { Reaction } from "/client/api";
 import { Tags } from "/lib/collections";
+import { playTour } from "/imports/plugins/included/tour/tour";
 
 Template.CoreNavigationBar.onCreated(function () {
   this.state = new ReactiveDict();
@@ -69,6 +70,17 @@ Template.CoreNavigationBar.helpers({
       onToggleMenu(callback) {
         // Register the callback
         instance.toggleMenuCallback = callback;
+      }
+    };
+  },
+  ComponentTourButton() {
+    return {
+      component: FlatButton,
+      kind: "flat",
+      label: "Take a Tour",
+      icon: "fa fa-rocket",
+      onClick() {
+        playTour();
       }
     };
   }
