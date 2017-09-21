f0a3d94b0d2e1ffcdc42c1f187e3d3c0946b2350
noordean <ebroyeem90@gmail.com>
2017-09-21 10:15:48 +0100

    bug(bug-fix): fix paystack secret keys
    - add pauldowman:dotenv to meteor
    - write paystack payment test
    
    [Fixes]

--- imports/plugins/included/paystack/server/methods/paystack.js
+++ imports/plugins/included/paystack/server/methods/paystack.js
@@ -1,16 +1,10 @@
 import { Meteor } from "meteor/meteor";
-import * as Collections from "/lib/collections";
-import { Reaction } from "/server/api";
 
 Meteor.methods({
   "paystack/getKeys"() {
-    const paystack = Collections.Packages.findOne({
-      name: "paystack",
-      shopId: Reaction.getShopId()
-    });
     return {
-      public: paystack.settings.publicKey,
-      secret: paystack.settings.secretKey
+      public: process.env.publicKey,
+      secret: process.env.secretKey
     };
   }
 });
