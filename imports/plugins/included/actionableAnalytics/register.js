36eb08db98143027d49abd1bbaf8dfde92a51f80
tundewritescode <tundewrites@gmail.com>
2017-09-25 12:49:36 +0100

    fix shop id

---
+++ imports/plugins/included/actionableAnalytics/register.js
@@ -0,0 +1,25 @@
+import { Reaction } from "/server/api";
+
+Reaction.registerPackage({
+  label: "Analytics",
+  name: "reaction-actionable-analytics",
+  icon: "fa fa-bar-chart",
+  autoEnable: true,
+  settings: {
+    name: "ActionableAnalytics"
+  },
+  registry: [
+    {
+      route: "/dashboard/actionable-analytics",
+      provides: "dashboard",
+      name: "actionableAnalytics",
+      label: "Analytics",
+      description: "Click to see your store's analytics",
+      icon: "fa fa-bar-chart",
+      priority: 1,
+      container: "core",
+      workflow: "coreDashboardWorkflow",
+      template: "actionableAnalytics"
+    }
+  ]
+});
