1d867873dcfd63a3eac2d26ff0c85311142019c1
tundewritescode <tundewrites@gmail.com>
2017-09-25 18:56:26 +0100

    test(filtering, sorting): e2e test for filtering and sorting search result
    - end to end test for filtering and sorting of search result
    
    [Finishes #150749928]

--- tests/acceptance-tests/test/specs/smoke-tests/search-ordering-filtering-test.js
+++ tests/acceptance-tests/test/specs/smoke-tests/search-ordering-filtering-test.js
@@ -3,7 +3,7 @@ const yaml = require("js-yaml");
 const fs = require("fs");
 const expect = require("chai").expect;
 
-beforeEach(function () {
+beforeEach(() => {
   const browserConfig = yaml.safeLoad(fs.readFileSync("./tests/acceptance-tests/config/settings.yml", "utf8"));
   const baseUrl = browserConfig.base_url.toString();
   browser.url(baseUrl);
@@ -11,23 +11,45 @@ beforeEach(function () {
 
 describe("sorting", function () {
   it("searches for Item and sort by highest or lowest price and filter by vendor or price", () => {
-    const searchQuery = "w";
-    browser.waitForExist(".navbar-items", "5000");
-    browser.waitForExist(".search", "5000");
+    const searchQuery = "o";
+    browser.waitForExist(".navbar-items");
+    browser.waitForExist(".search");
     browser.click(".search");
-    browser.waitForExist("#search-input", "5000");
+    browser.waitForExist("#search-input");
     browser.setValue("#search-input", searchQuery);
     browser.scroll(0, 200);
     browser.click(".filter-search");
-    browser.waitForExist("#price-filter", "5000");
-    browser.click("#price-filter");
-    browser.waitForExist("#firstPrice", "5000");
-    browser.click("#firstPrice");
-    browser.waitForExist("#brand-filter", "5000");
+
     browser.click("#brand-filter");
-    browser.waitForExist(".product-primary-images", "5000");
-    browser.waitForExist("#product-title", "5000");
-    expect(browser.getText("#product-title")).to.contain("BASIC REACTION PRODUCT");
+    browser.pause(2000);
+    browser.click("//option[text()='Apple']");
+    browser.waitForExist("#zu8fMi9jEhmWw2c7K");
+    browser.click("#brand-filter");
+    browser.waitForExist(".product-primary-images");
+    browser.waitForExist("#product-title");
+
+    expect(browser.getText("#product-title")).to.not.equal("GO-KART COSTUME");
+    expect(browser.getText("#product-title")).to.contain("IPHONE X");
+    expect(browser.getText("#product-title")).to.contain("IPHONE 8S");
+
+    browser.click("//option[text()='All vendors']");
+    browser.waitForExist("//option[text()='All vendors']");
+    browser.click("//option[text()='Babatunde']");
+    browser.waitForExist(".product-primary-images");
+    browser.waitForExist("#product-title");
+
+    expect(browser.getText("#product-title")).to.contain("GO-KART COSTUME");
+    expect(browser.getText("#product-title")).to.not.equal("IPHONE X");
+    expect(browser.getText("#product-title")).to.not.equal("IPHONE 8S");
+
+    browser.click("//option[text()='All vendors']");
+    browser.click("#price-filter");
+    browser.waitForExist("#firstPrice");
+    browser.click("#firstPrice");
+
+    expect(browser.getText("#product-title")).to.contain("IPHONE X");
+    expect(browser.getText("#product-title")).to.contain("IPHONE 8S");
+    expect(browser.getText("#product-title")).to.not.equal("GO-KART COSTUME");
   });
 });
 
